d9485133b1df2436db0cb8e63d5fe423ddedba72
Matthew.Greco <matthew.greco85@gmail.com>
2019-08-18 04:42:29 -0500

    So many files missing

---
+++ js/break_and_continue.js
@@ -0,0 +1,16 @@
+
+
+// -------------------------exercise 6---------------------------
+var userNumb = prompt('Pick an ODD number between 1-50 that we will skip.');
+
+for (var  i = 1; i <= 50 ; i += 1) {
+
+    if (i !== parseInt(userNumb)) {
+        console.log('Here is an Odd number ' + i);
+    } else {
+        console.log('Yikes, we are skipping: ' + userNumb);
+    }
+    if (i % 2 === 0) {
+        continue;
+    }
+}
